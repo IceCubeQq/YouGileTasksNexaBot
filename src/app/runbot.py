7a5a6b065e5aac7e82c1460d7cfda24a7fdf0157
IceCubeQq <icecubepq@gmail.com>
2026-02-16 20:43:37 +0500

    Update runbot.py

--- src/app/runbot.py
+++ src/app/runbot.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from django.core.management.base import BaseCommand
 from app.internal.bot import create_bot
 
@@ -7,4 +9,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Успешный старт"))
         bot = create_bot()
+
         bot.run_polling()
